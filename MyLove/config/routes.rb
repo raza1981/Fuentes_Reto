31f39f4fb8fbf728237ea155e02453f12799d8aa
harley213 <harley213@hotmail.com>
2018-08-31 20:05:53 -0500

    ajuste estilos

--- MyLove/config/routes.rb
+++ MyLove/config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :message_types
   resources :rols
   resources :contacts
+
+  root 'reports#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
